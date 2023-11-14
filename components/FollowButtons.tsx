b7f724eae1c14a3e17dc76c3dcb7c128bf889859
Jaron Heard <jaronheard@gmail.com>
2023-11-13 16:14:12 -0800

    add notifications to buttons

--- components/FollowButtons.tsx
+++ components/FollowButtons.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/nextjs";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
@@ -39,6 +39,14 @@ export function FollowEventDropdownButton({
 
     const event = await response.json();
 
+    if (following) {
+      toast.success("Event unsaved.");
+    }
+
+    if (!following) {
+      toast.success("Event saved.");
+    }
+
     // This forces a cache invalidation.
     router.refresh();
   }
@@ -71,80 +79,80 @@ export function FollowEventDropdownButton({
   );
 }
 
-export function FollowEventButton({
-  eventId,
-  following,
-}: {
-  eventId: string;
-  following: boolean;
-}) {
-  const router = useRouter();
-  const pathName = usePathname();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+// export function FollowEventButton({
+//   eventId,
+//   following,
+// }: {
+//   eventId: string;
+//   following: boolean;
+// }) {
+//   const router = useRouter();
+//   const pathName = usePathname();
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  async function followUnfollowEvent() {
-    setIsLoading(true);
+//   async function followUnfollowEvent() {
+//     setIsLoading(true);
 
-    const response = await fetch("/api/events/follow", {
-      method: following ? "DELETE" : "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        eventId: eventId,
-      }),
-    });
+//     const response = await fetch("/api/events/follow", {
+//       method: following ? "DELETE" : "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         eventId: eventId,
+//       }),
+//     });
 
-    setIsLoading(false);
+//     setIsLoading(false);
 
-    if (!response?.ok) {
-      return toast.error("Your event was not saved. Please try again.");
-    }
+//     if (!response?.ok) {
+//       return toast.error("Your event was not saved. Please try again.");
+//     }
 
-    const event = await response.json();
+//     const event = await response.json();
 
-    // This forces a cache invalidation.
-    router.refresh();
-  }
+//     // This forces a cache invalidation.
+//     router.refresh();
+//   }
 
-  return (
-    <>
-      <SignedIn>
-        {isLoading && (
-          <Button disabled size="sm">
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Please wait
-          </Button>
-        )}
-        {!isLoading && (
-          <Button onClick={followUnfollowEvent} size="sm">
-            {following && (
-              <>
-                <Check className="mr-2 h-4 w-4" />
-                Event Saved
-              </>
-            )}
-            {!following && (
-              <>
-                <Plus className="mr-2 h-4 w-4" />
-                Save Event
-              </>
-            )}
-          </Button>
-        )}
-      </SignedIn>
-      <SignedOut>
-        {/* TODO: instead convert from the AddToCalendarButtonProps */}
-        <SignInButton
-          afterSignInUrl={`${process.env.NEXT_PUBLIC_URL}/${pathName}`}
-          afterSignUpUrl={`${process.env.NEXT_PUBLIC_URL}/${pathName}`}
-        >
-          <Button>Sign in to follow</Button>
-        </SignInButton>
-      </SignedOut>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <SignedIn>
+//         {isLoading && (
+//           <Button disabled size="sm">
+//             <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+//             Please wait
+//           </Button>
+//         )}
+//         {!isLoading && (
+//           <Button onClick={followUnfollowEvent} size="sm">
+//             {following && (
+//               <>
+//                 <Check className="mr-2 h-4 w-4" />
+//                 Event Saved
+//               </>
+//             )}
+//             {!following && (
+//               <>
+//                 <Plus className="mr-2 h-4 w-4" />
+//                 Save Event
+//               </>
+//             )}
+//           </Button>
+//         )}
+//       </SignedIn>
+//       <SignedOut>
+//         {/* TODO: instead convert from the AddToCalendarButtonProps */}
+//         <SignInButton
+//           afterSignInUrl={`${process.env.NEXT_PUBLIC_URL}/${pathName}`}
+//           afterSignUpUrl={`${process.env.NEXT_PUBLIC_URL}/${pathName}`}
+//         >
+//           <Button>Sign in to follow</Button>
+//         </SignInButton>
+//       </SignedOut>
+//     </>
+//   );
+// }
 
 export function FollowUserButton({
   userId,
@@ -177,6 +185,14 @@ export function FollowUserButton({
     }
 
     const user = await response.json();
+
+    if (following) {
+      toast.success("Unfollowed user.");
+    }
+
+    if (!following) {
+      toast.success("Followed user.");
+    }
 
     // This forces a cache invalidation.
     router.refresh();
@@ -242,6 +258,14 @@ export function FollowListButton({
     }
 
     const user = await response.json();
+
+    if (following) {
+      toast.success("Unfollowed list.");
+    }
+
+    if (!following) {
+      toast.success("Followed list.");
+    }
 
     // This forces a cache invalidation.
     router.refresh();
