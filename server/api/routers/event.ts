b87850ef2ee7b9530ccaf9452f2307cb6cb54ea4
Jaron Heard <jaronheard@gmail.com>
2023-11-27 19:49:10 -0800

    eventId page

--- server/api/routers/event.ts
+++ server/api/routers/event.ts
@@ -123,4 +123,57 @@ export const eventRouter = createTRPCRouter({
       },
     });
   }),
+  getPossibleDuplicates: publicProcedure
+    .input(z.object({ startDateTime: z.date() }))
+    .query(({ ctx, input }) => {
+      const { startDateTime } = input;
+      // start date time should be within 1 hour of the start date time of the event
+      const startDateTimeLowerBound = new Date(startDateTime);
+      startDateTimeLowerBound.setHours(startDateTime.getHours() - 1);
+      const startDateTimeUpperBound = new Date(startDateTime);
+      startDateTimeUpperBound.setHours(startDateTime.getHours() + 1);
+
+      const possibleDuplicateEvents = ctx.db.event.findMany({
+        where: {
+          startDateTime: {
+            gte: startDateTimeLowerBound,
+            lte: startDateTimeUpperBound,
+          },
+        },
+        select: {
+          startDateTime: true,
+          endDateTime: true,
+          id: true,
+          event: true,
+          createdAt: true,
+          userId: true,
+          User: true,
+          FollowEvent: true,
+          Comment: true,
+          visibility: true,
+        },
+      });
+      return possibleDuplicateEvents;
+    }),
+  get: publicProcedure
+    .input(z.object({ eventId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.event.findUnique({
+        where: {
+          id: input.eventId,
+        },
+        select: {
+          startDateTime: true,
+          endDateTime: true,
+          id: true,
+          event: true,
+          createdAt: true,
+          userId: true,
+          User: true,
+          FollowEvent: true,
+          Comment: true,
+          visibility: true,
+        },
+      });
+    }),
 });
