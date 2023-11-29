f92c2580f95b32d8d51544a8bef54a5c579ad744
Jaron Heard <jaronheard@gmail.com>
2023-11-28 17:30:03 -0800

    remove unused code

--- components/FollowButtons.tsx
+++ components/FollowButtons.tsx
@@ -2,7 +2,6 @@
 
 import { SignedIn } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { Check, Loader2, Plus } from "lucide-react";
 import { Button } from "./ui/button";
@@ -71,81 +70,6 @@ export function FollowEventDropdownButton({
     </>
   );
 }
-
-// export function FollowEventButton({
-//   eventId,
-//   following,
-// }: {
-//   eventId: string;
-//   following: boolean;
-// }) {
-//   const router = useRouter();
-//   const pathName = usePathname();
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-//   async function followUnfollowEvent() {
-//     setIsLoading(true);
-
-//     const response = await fetch("/api/events/follow", {
-//       method: following ? "DELETE" : "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         eventId: eventId,
-//       }),
-//     });
-
-//     setIsLoading(false);
-
-//     if (!response?.ok) {
-//       return toast.error("Your event was not saved. Please try again.");
-//     }
-
-//     const event = await response.json();
-
-//     // This forces a cache invalidation.
-//     router.refresh();
-//   }
-
-//   return (
-//     <>
-//       <SignedIn>
-//         {isLoading && (
-//           <Button disabled size="sm">
-//             <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-//             Please wait
-//           </Button>
-//         )}
-//         {!isLoading && (
-//           <Button onClick={followUnfollowEvent} size="sm">
-//             {following && (
-//               <>
-//                 <Check className="mr-2 h-4 w-4" />
-//                 Event Saved
-//               </>
-//             )}
-//             {!following && (
-//               <>
-//                 <Plus className="mr-2 h-4 w-4" />
-//                 Save Event
-//               </>
-//             )}
-//           </Button>
-//         )}
-//       </SignedIn>
-//       <SignedOut>
-//         {/* TODO: instead convert from the AddToCalendarButtonProps */}
-//         <SignInButton
-//           afterSignInUrl={`${process.env.NEXT_PUBLIC_URL}/${pathName}`}
-//           afterSignUpUrl={`${process.env.NEXT_PUBLIC_URL}/${pathName}`}
-//         >
-//           <Button>Sign in to follow</Button>
-//         </SignInButton>
-//       </SignedOut>
-//     </>
-//   );
-// }
 
 export function FollowUserButton({
   userId,
