3f89fc1cc2c39d00a1ab65e3bec1a0e0ca73b5e2
Jaron Heard <jaronheard@gmail.com>
2024-01-13 12:18:42 +0900

    remove leftover relative

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center">
+    <div className="mx-auto flex min-h-screen max-w-5xl flex-col items-center justify-center">
       <Toaster />
       <Header />
       <main className="my-12 flex w-full flex-1 flex-col items-center justify-center sm:my-20 sm:px-4">
