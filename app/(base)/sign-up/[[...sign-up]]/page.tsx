ddc50da60ac8b8eeabf10e0a3b5b86edbdc1c168
Jaron Heard <jaronheard@gmail.com>
2024-01-29 13:54:02 -0800

    clerk width fix

--- app/(base)/sign-up/[[...sign-up]]/page.tsx
+++ app/(base)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen w-screen items-center justify-center bg-white">
+    <div className="flex min-h-screen w-full items-center justify-center bg-white">
       <SignUp />
     </div>
   );
