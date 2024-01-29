0664eb7c041b51166bf07bd5d35b365b59e57657
Jaron Heard <jaronheard@gmail.com>
2024-01-29 13:40:22 -0800

    wrap signin signup

--- app/(base)/sign-in/[[...sign-in]]/page.tsx
+++ app/(base)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex min-h-screen w-screen items-center justify-center bg-white">
+      <SignIn />
+    </div>
+  );
 }
--- app/(base)/sign-up/[[...sign-up]]/page.tsx
+++ app/(base)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex min-h-screen w-screen items-center justify-center bg-white">
+      <SignUp />
+    </div>
+  );
 }
