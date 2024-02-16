72e17db6dafd107c524e4b0ddd860878b20c7d79
Jaron Heard <jaronheard@gmail.com>
2024-02-16 15:31:16 -0800

    no edge

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { TRPCReactProvider } from "@/trpc/react";
 
 // edge causes sigkill on vercel about 50% of the time
 // workaround is to use VERCEL_FORCE_NO_BUILD_CACHE=1
-export const runtime = "edge";
+// export const runtime = "edge";
 export const preferredRegion = "pdx1";
 
 const kalam = Kalam({
