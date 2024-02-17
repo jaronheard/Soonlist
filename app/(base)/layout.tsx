7b693984aaa0b58961d7d93766578426d0c855d1
Jaron Heard <jaronheard@gmail.com>
2024-02-16 18:10:37 -0800

    adjust min height and sign up pages

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Header />
       <Toaster />
-      <main className="mx-auto min-h-screen w-full max-w-7xl px-6 py-16 lg:px-8 lg:py-24">
+      <main className="mx-auto min-h-[calc(100vh-4.5rem)] w-full max-w-7xl px-6 py-16 sm:min-h-[calc(100vh-5.75rem)] lg:px-8 lg:py-24">
         {children}
       </main>
       <Footer />
--- app/(base)/sign-up/[[...sign-up]]/page.tsx
+++ app/(base)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-white">
+    <div className="flex w-full justify-center bg-white">
       <SignUp afterSignUpUrl={`${process.env.NEXT_PUBLIC_URL}/get-started`} />
     </div>
   );
