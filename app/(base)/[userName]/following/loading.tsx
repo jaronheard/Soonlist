263fd7db8ac6b83907e143b30657c57a37b1b1ae
Jaron Heard <jaronheard@gmail.com>
2024-02-16 17:24:41 -0800

    use common component for loading

--- app/(base)/[userName]/following/loading.tsx
+++ app/(base)/[userName]/following/loading.tsx
@@ -1,8 +1,5 @@
+import FullPageLoadingSpinner from "@/components/FullPageLoadingSpinner";
+
 export default function Loading() {
-  return (
-    <div className="fixed inset-x-0 bottom-0 top-[4.5rem] z-50 flex items-center justify-center bg-white opacity-90 sm:top-[5.75rem]">
-      {/* Notice the z-index here */}
-      <div className="size-6 animate-spin rounded-full border-b-2 border-gray-400"></div>
-    </div>
-  );
+  return <FullPageLoadingSpinner />;
 }
---
+++ components/FullPageLoadingSpinner.tsx
@@ -0,0 +1,10 @@
+// this is the a full page loading spinner
+
+export default function FullPageLoadingSpinner() {
+  return (
+    <div className="fixed inset-x-0 bottom-0 top-[4.5rem] z-50 flex items-center justify-center bg-white opacity-90 sm:top-[5.75rem]">
+      {/* Notice the z-index here */}
+      <div className="size-6 animate-spin rounded-full border-b-2 border-gray-400"></div>
+    </div>
+  );
+}
