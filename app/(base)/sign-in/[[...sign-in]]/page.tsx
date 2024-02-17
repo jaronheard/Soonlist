ed680b87cedfcd6be681f32487a0c9cebbb66811
Jaron Heard <jaronheard@gmail.com>
2024-02-16 18:10:48 -0800

    minheight
    * remove unused
    
    * remove unused component
    
    * remove unused code
    
    * make footer client component
    
    * adjust min height and sign up pages

--- app/(base)/sign-in/[[...sign-in]]/page.tsx
+++ app/(base)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-white">
+    <div className="flex w-full justify-center bg-white">
       <SignIn />
     </div>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { CircleDollarSign } from "lucide-react";
