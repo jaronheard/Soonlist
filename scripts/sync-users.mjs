aa9e570e480c8036f4127560cd52786320294602
Jaron Heard <jaronheard@gmail.com>
2024-02-19 10:12:24 -0800

    add script and submodule

--- scripts/sync-users.mjs
+++ scripts/sync-users.mjs
@@ -1,57 +1,69 @@
-// TODO: update this for drizzle
+import "dotenv/config";
 
-// // Import the necessary libraries
-// import { PrismaClient } from "@prisma/client";
-// import { Clerk } from "@clerk/clerk-sdk-node";
+// Import the necessary libraries
+import { Clerk } from "@clerk/clerk-sdk-node";
+import fs from "fs";
 
-// // Initialize Prisma Client
-// const prisma = new PrismaClient();
+// Set up Clerk client with your Clerk Backend API key
+const clerkProd = Clerk({ secretKey: process.env.CLERK_SECRET_KEY_PROD });
+const clerkDev = Clerk({ secretKey: process.env.CLERK_SECRET_KEY_DEV });
 
-// // Set up Clerk client with your Clerk Backend API key
-// const clerk = Clerk({ apiKey: process.env.CLERK_SECRET_KEY });
+async function getClerkUsersProd() {
+  try {
+    // Fetch users from Clerk
+    const users = await clerkProd.users.getUserList({ limit: 500 });
+    console.log(`Fetched ${users.length} users from Clerk.`);
+    return users;
+  } catch (error) {
+    console.error("Failed to fetch users from production:", error);
+    throw error; // Rethrow the error if you want to handle it further up the chain.
+  }
+}
 
-// async function fetchClerkUsersAndSyncRelations() {
-//   // Fetch users from Clerk
-//   const users = await clerk.users.getUserList({ limit: 500 });
-//   console.log(`Fetched ${users.length} users from Clerk.`);
+async function deleteAllClerkUsersDev() {
+  try {
+    // Fetch users from Clerk
+    const users = await clerkDev.users.getUserList({ limit: 500 });
+    console.log(`Fetched ${users.length} users from Clerk.`);
+    let deletedUsersCount = 0; // Initialize as let to increment it.
+    // Delete all users from Clerk
+    for (const user of users) {
+      await clerkDev.users.deleteUser(user.id);
+      console.log(`Deleted user ${user.id} from Clerk.`);
+      deletedUsersCount++;
+    }
+    console.log(`Deleted ${deletedUsersCount} users from Clerk.`);
+  } catch (error) {
+    console.error("Failed to delete users from development:", error);
+    throw error; // Rethrow the error if you want to handle it further up the chain.
+  }
+}
 
-//   for (const user of users) {
-//     // Upsert user to handle both new and existing records
-//     await prisma.user.upsert({
-//       where: {
-//         email: user?.emailAddresses[0]?.emailAddress,
-//       },
-//       create: {
-//         id: user.id,
-//         username: user?.username || "",
-//         email: user?.emailAddresses[0]?.emailAddress || "",
-//         displayName:
-//           user?.firstName || user?.lastName
-//             ? `${user?.firstName || ""} ${user?.lastName || ""}`.trim()
-//             : "",
-//         imageUrl: user?.imageUrl || "",
-//       },
-//       update: {
-//         id: user.id,
-//         username: user?.username || "",
-//         email: user?.emailAddresses[0]?.emailAddress || "",
-//         displayName:
-//           user?.firstName || user?.lastName
-//             ? `${user?.firstName || ""} ${user?.lastName || ""}`.trim()
-//             : "",
-//         imageUrl: user?.imageUrl || "",
-//         updatedAt: new Date(),
-//       },
-//     });
-//     console.log(`User ${user.id} ${user.username} synchronized successfully.`);
-//   }
+async function saveUsersToFile(users) {
+  try {
+    const usersJson = JSON.stringify(users);
+    fs.writeFileSync("scripts/migration-script/users.json", usersJson);
+    console.log(`Saved ${users.length} users to users.json`);
+  } catch (error) {
+    console.error("Failed to save users to file:", error);
+    throw error; // Rethrow the error if you want to handle it further up the chain.
+  }
+}
 
-//   await prisma.$disconnect();
-// }
+async function main() {
+  try {
+    // Fetch users from Clerk
+    const users = await getClerkUsersProd();
+    // Save users to file
+    await saveUsersToFile(users);
+    // Delete all users from Clerk
+    // await deleteAllClerkUsersDev();
+    console.log("Saved production users to file complete.");
+    await deleteAllClerkUsersDev();
+    console.log("Deleted development users complete.");
+  } catch (error) {
+    console.error("An error occurred during the sync process:", error);
+  }
+}
 
-// // Run the function and handle any errors
-// fetchClerkUsersAndSyncRelations()
-//   .then(() => console.log("Users and relations synchronized successfully."))
-//   .catch((error) =>
-//     console.error("Error synchronizing users and relations:", error)
-//   );
+main();
