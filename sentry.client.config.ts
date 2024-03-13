0cf7b240f39cf98b3d03a55c09f6478baafd0252
Jaron Heard <jaronheard@gmail.com>
2024-03-13 16:45:58 -0700

    sentry sessions
    * upgrade sentry
    
    * untreeshake sentry
    
    * enable replays

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,8 +8,19 @@ import { posthog } from "posthog-js";
 Sentry.init({
   dsn: "https://35d541c34f3a87134429ac75e6513a16@o4503934125998080.ingest.sentry.io/4506458761396224",
   tracesSampleRate: 1.0,
+  // Set the session sample rate to 0 to disable capturing entire sessions
+  replaysSessionSampleRate: 0,
+
+  // Set the sample rate to 1.0 to capture replays only when an error occurs
+  replaysOnErrorSampleRate: 1.0,
   integrations: [
     new posthog.SentryIntegration(posthog, "soonlist", 4506458761396224),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    Sentry.replayIntegration({
+      // Additional SDK configuration goes in here, for example:
+      // maskAllText: true,
+      blockAllMedia: true,
+    }),
   ],
   enabled: process.env.NODE_ENV === "production",
 });
