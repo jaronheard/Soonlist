8d519283e41c48fd4434639ae4f7af9b638c8941
Jaron Heard <jaronheard@gmail.com>
2024-03-13 16:43:31 -0700

    untreeshake sentry

--- next.config.js
+++ next.config.js
@@ -36,11 +36,11 @@ const nextConfig = {
     config.plugins.push(
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       new webpack.DefinePlugin({
-        __SENTRY_DEBUG__: false,
-        __SENTRY_TRACING__: false,
-        __RRWEB_EXCLUDE_IFRAME__: true,
-        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
-        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+        // __SENTRY_DEBUG__: false,
+        // __SENTRY_TRACING__: false,
+        // __RRWEB_EXCLUDE_IFRAME__: true,
+        // __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        // __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
       })
     );
 
