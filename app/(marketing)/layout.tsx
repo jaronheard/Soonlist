09131027774a6b22f3810e3c7f4ccea45bdaad03
Jaron Heard <jaronheard@gmail.com>
2024-03-13 18:49:39 -0700

    is main the issue

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,14 +2,14 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-// import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      {/* <Toaster />
-      <main className="min-h-screen">{children}</main> */}
+      <Toaster />
+      {children}
       <Footer />
     </div>
   );
