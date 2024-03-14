2f38441d65da0611dcf0dcdaf0754228a89dc872
Jaron Heard <jaronheard@gmail.com>
2024-03-13 18:10:33 -0700

    temp: remove toaster

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,13 +2,13 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { Toaster } from "@/components/ui/sonner";
+// import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      <Toaster />
+      {/* <Toaster /> */}
       <main className="min-h-screen">{children}</main>
       <Footer />
     </div>
