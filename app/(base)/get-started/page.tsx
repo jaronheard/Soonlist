ae060780cc371e3eb8d1b1396876d47fb2383b42
Jaron Heard <jaronheard@gmail.com>
2024-03-14 13:30:24 -0700

    log errors in get-started

--- app/(base)/get-started/page.tsx
+++ app/(base)/get-started/page.tsx
@@ -14,22 +14,24 @@ export const metadata = {
 export default async function Page() {
   const activeUser = await currentUser();
   if (!activeUser) {
+    console.error("No currentUser found in get-started/page.tsx");
     return null;
   }
   const user = await api.user.getByUsername.query({
     userName: activeUser.username || "",
   });
   if (!user) {
+    console.error("No user found in get-started/page.tsx");
     return null;
   }
 
   return (
     <OnboardingTabs
       additionalInfo={{
-        bio: user.bio || undefined,
-        publicEmail: user.publicEmail || undefined,
-        publicPhone: user.publicPhone || undefined,
-        publicInsta: user.publicInsta || undefined,
+        bio: user?.bio || undefined,
+        publicEmail: user?.publicEmail || undefined,
+        publicPhone: user?.publicPhone || undefined,
+        publicInsta: user?.publicInsta || undefined,
         publicWebsite: user?.publicWebsite || undefined,
       }}
     />
