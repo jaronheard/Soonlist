2d82a587060ef4620d27cbb847fbb49298bf6f5e
Jaron Heard <jaronheard@gmail.com>
2024-03-13 18:39:20 -0700

    ensure in main

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-// import Footer from "@/components/Footer";
-// import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 // import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      {/* <Header />
-      <Toaster /> */}
-      <main className="min-h-screen">{children}</main>
-      {/* <Footer /> */}
+      <Header />
+      {/* <Toaster />
+      <main className="min-h-screen">{children}</main> */}
+      <Footer />
     </div>
   );
 }
