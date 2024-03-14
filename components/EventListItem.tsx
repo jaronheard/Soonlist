1e46033e4879a5c19d0ff1c7e2bfffc4ac420ccf
Jaron Heard <jaronheard@gmail.com>
2024-03-14 11:06:22 -0700

    client render dates

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useUser } from "@clerk/nextjs";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ArrowRight, EyeOff } from "lucide-react";
 import { DeleteButton } from "./DeleteButton";
 import { EditButton } from "./EditButton";
@@ -208,6 +208,12 @@ function EventDetails({
   EventActionButtons?: React.ReactNode;
 }) {
   const { timezone: userTimezone } = useContext(TimezoneContext);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   if (!startDate || !endDate) {
     console.error("startDate or endDate is missing");
     return null;
@@ -237,8 +243,8 @@ function EventDetails({
   return (
     <div className="flex w-full flex-col items-start justify-center gap-2">
       {/* duplicated with Event */}
-      {/* <div className="flex-start flex gap-2 pr-12 text-lg font-medium leading-none">
-        {eventTimesAreDefined(startTime, endTime) && (
+      <div className="flex-start flex gap-2 pr-12 text-lg font-medium leading-none">
+        {isClient && eventTimesAreDefined(startTime, endTime) && (
           <>
             <div className="flex-wrap text-neutral-2">
               {startDateInfo?.dayOfWeek.substring(0, 3)}
@@ -251,7 +257,7 @@ function EventDetails({
             </div>
           </>
         )}
-      </div> */}
+      </div>
       {/* end duplicated with Event */}
       <div className="flex w-full flex-col items-start gap-2">
         <Link
