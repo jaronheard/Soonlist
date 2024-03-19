9cb436ac95cbe6b473590c921392e56f6981aee2
Jaron Heard <jaronheard@gmail.com>
2024-03-18 20:21:37 -0700

    disable vercel toolbar

--- components/VercelToolbar.tsx
+++ components/VercelToolbar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
-import { VercelToolbar as Toolbar } from "@vercel/toolbar/next";
+// disabled for now; we don't have a use case for this yet
+
+// import { useUser } from "@clerk/nextjs";
+// import { VercelToolbar as Toolbar } from "@vercel/toolbar/next";
 
 export function VercelToolbar() {
-  const { user } = useUser();
-  const roles = user?.unsafeMetadata.roles as string[] | undefined;
-  const toolbar = roles?.includes("toolbar");
-  const isDev = process.env.NODE_ENV === "development";
-  const showToolbar = isDev || toolbar;
-  return showToolbar ? <Toolbar /> : null;
+  return null;
+  // const { user } = useUser();
+  // const roles = user?.unsafeMetadata.roles as string[] | undefined;
+  // const toolbar = roles?.includes("toolbar");
+  // const isDev = process.env.NODE_ENV === "development";
+  // const showToolbar = isDev || toolbar;
+  // return showToolbar ? <Toolbar /> : null;
 }
