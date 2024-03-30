d9be8743b0671aff66740e1a681263d486713b92
Jaron Heard <jaronheard@gmail.com>
2024-03-29 19:55:29 -0700

    remove unused dropdown

--- app/(base)/onboarding/OnboardingContinueButton.tsx
+++ app/(base)/onboarding/OnboardingContinueButton.tsx
@@ -24,7 +24,7 @@ export default function OnboardingContinueButton() {
           afterSignInUrl={signedOutUrl}
           afterSignUpUrl={signedOutUrl}
         >
-          <button className="hover:bg-black/80 mt-12 block w-full rounded-xl bg-black px-4 py-2 font-medium text-white sm:mt-16">
+          <button className="mt-12 block w-full rounded-xl bg-black px-4 py-2 font-medium text-white hover:bg-black/80 sm:mt-16">
             Sign up &rarr;
           </button>
         </SignUpButton>
@@ -32,7 +32,7 @@ export default function OnboardingContinueButton() {
       <SignedIn>
         <Link
           href={signedInPath}
-          className="hover:bg-black/80 mt-12 block w-full rounded-xl bg-black px-4 py-2 text-center font-medium text-white sm:mt-16"
+          className="mt-12 block w-full rounded-xl bg-black px-4 py-2 text-center font-medium text-white hover:bg-black/80 sm:mt-16"
         >
           Continue &rarr;
         </Link>
