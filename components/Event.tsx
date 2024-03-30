348eb0605ab46342dbd15f106f9184b7565d77a5
Jaron Heard <jaronheard@gmail.com>
2024-03-29 20:02:46 -0700

    add personal note (#163)
    * add personal note
    
    * adjust rgb colors
    
    * update additional colors
    
    * fix design
    
    * remove unused dropdown
    
    * remove unused call to action

--- components/Event.tsx
+++ components/Event.tsx
@@ -11,6 +11,7 @@ import { ShareButton } from "./ShareButton";
 import { FollowEventButton } from "./FollowButtons";
 import { type EventWithUser } from "./EventList";
 import ListCard from "./ListCard";
+import PersonalNote from "./PersonalNote";
 import UserAllEventsCard from "./UserAllEventsCard";
 import {
   type User,
@@ -199,14 +200,27 @@ export function Event(props: EventProps) {
     setIsClient(true);
   }, []);
 
-  const { user, eventFollows, id, event, image, singleEvent, children, lists } =
-    props;
+  const {
+    user,
+    eventFollows,
+    id,
+    event,
+    image,
+    singleEvent,
+    children,
+    lists,
+  } = props;
   const roles = clerkUser?.unsafeMetadata.roles as string[] | undefined;
   const isSelf =
     clerkUser?.id === user?.id || clerkUser?.externalId === user?.id;
   const isOwner = isSelf || roles?.includes("admin");
   const isFollowing = !!eventFollows.find(
-    (item) => item.userId === clerkUser?.id
+    (item) =>
+      clerkUser?.id === item.userId || clerkUser?.externalId === item.userId
+  );
+  const comment = props.comments?.findLast(
+    (item) =>
+      clerkUser?.id === item.userId || clerkUser?.externalId === item.userId
   );
   const hasLists = user && lists && lists.length > 0;
 
@@ -284,6 +298,7 @@ export function Event(props: EventProps) {
                 </Link>
               )}
             </div>
+            <PersonalNote text={comment?.content} />
             {!hasLists && user && (
               <UserAllEventsCard
                 username={user.username}
---
+++ components/PersonalNote.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const colors = [
+  // "bg-accent-blue",
+  "bg-accent-red/40",
+  "bg-accent-orange/40",
+  "bg-accent-green/40",
+];
+
+const getRainbowColorFromString = (initials: string) => {
+  return colors[initials.charCodeAt(0) % colors.length];
+};
+
+export default function PersonalNote({
+  text,
+  className,
+}: {
+  text?: string;
+  className?: string;
+}) {
+  if (!text) return null;
+  return (
+    <div
+      className={cn(
+        "relative w-max max-w-full rounded-3xl p-2 px-3 pt-3 align-text-top font-heading text-xl font-semibold text-neutral-2",
+        getRainbowColorFromString(text),
+        className
+      )}
+    >
+      <div className="absolute -left-2 top-2 font-heading text-4xl font-normal text-primary">
+        &ldquo;
+      </div>
+      {text}
+    </div>
+  );
+}
