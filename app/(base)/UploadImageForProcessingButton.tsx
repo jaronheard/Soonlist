ef330e79ae7f998b255c91114df93aa636f4bfa8
Jaron Heard <jaronheard@gmail.com>
2024-04-04 01:32:53 -0400

    back to router.push

--- app/(base)/UploadImageForProcessingButton.tsx
+++ app/(base)/UploadImageForProcessingButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { UploadDropzone } from "@bytescale/upload-widget-react";
+import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { TimezoneContext } from "@/context/TimezoneContext";
 import { bytescaleWidgetOptions } from "@/components/ImageUpload";
@@ -7,6 +8,7 @@ import { bytescaleWidgetOptions } from "@/components/ImageUpload";
 const widgetOptions = { ...bytescaleWidgetOptions };
 
 export const UploadImageForProcessingButton = () => {
+  const router = useRouter();
   const { timezone } = useContext(TimezoneContext);
   return (
     <UploadDropzone
@@ -15,7 +17,7 @@ export const UploadImageForProcessingButton = () => {
         if (uploadedFiles.length > 0) {
           const filePath = uploadedFiles[0]?.filePath;
           if (filePath) {
-            window.location.href = `/new?filePath=${filePath}&timezone=${timezone}`;
+            router.push(`/new?filePath=${filePath}&timezone=${timezone}`);
           }
         }
       }}
