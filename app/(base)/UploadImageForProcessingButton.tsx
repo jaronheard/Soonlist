73e06a428f4d2fcd7a7ba7a92c97e49ea111d7c4
Jaron Heard <jaronheard@gmail.com>
2024-04-03 23:56:53 -0400

    adjust flow

--- app/(base)/UploadImageForProcessingButton.tsx
+++ app/(base)/UploadImageForProcessingButton.tsx
@@ -1,23 +1,21 @@
 "use client";
 import { UploadDropzone } from "@bytescale/upload-widget-react";
-import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { TimezoneContext } from "@/context/TimezoneContext";
 import { bytescaleWidgetOptions } from "@/components/ImageUpload";
 
-const widgetOptions = { ...bytescaleWidgetOptions, showFinishButton: true };
+const widgetOptions = { ...bytescaleWidgetOptions };
 
 export const UploadImageForProcessingButton = () => {
-  const router = useRouter();
   const { timezone } = useContext(TimezoneContext);
   return (
     <UploadDropzone
       options={widgetOptions}
-      onComplete={(files) => {
-        if (files.length > 0) {
-          const filePath = files[0]?.filePath;
+      onUpdate={({ uploadedFiles }) => {
+        if (uploadedFiles.length > 0) {
+          const filePath = uploadedFiles[0]?.filePath;
           if (filePath) {
-            router.push(`/new?filePath=${filePath}&timezone=${timezone}`);
+            window.location.href = `/new?filePath=${filePath}&timezone=${timezone}`;
           }
         }
       }}
