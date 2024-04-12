a0d6d4372055c0a0c83884b315c6ec718f0761e1
Jaron Heard <jaronheard@gmail.com>
2024-04-12 16:15:48 -0700

    fix unknown start time issue

--- lib/prompts.ts
+++ lib/prompts.ts
@@ -289,7 +289,7 @@ interface Event {
   name: string; // The event's name. Be specific and include any subtitle or edition. Do not include the location. Use proper capitalization, ignoring all caps in the source text.
   description: string; // Short description of the event, its significance, and what attendees can expect. If included in the source text, include the cost, allowed ages, rsvp details, performers, speakers, and any known times.
   startDate: string; // Start date in YYYY-MM-DD format.
-  startTime?: string; // Start time, only the time portion (HH:MM:SS) of ISO 8601 format. Do not include the date or time zone. Only omit if known to be an all-day event.
+  startTime?: string; // Start time, only the time portion (HH:MM:SS) of ISO 8601 format. Do not include the date or time zone. Infer based on event type if not specified. Only omit if known to be an all-day event. CANNOT BE UNKNOWN.
   endDate: string; // End date in YYYY-MM-DD format.
   endTime?: string; // Start time, only the time portion (HH:MM:SS) of ISO 8601 format. Do not include the date or time zone. Infer based on start time and event type if not specified. Only omit if known to be an all-day event. CANNOT BE UNKNOWN.
   timeZone: string; // Timezone in IANA format.
