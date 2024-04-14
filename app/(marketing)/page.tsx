bab5216990e80933cfccf82e383aa0c48879374c
Jaron Heard <jaronheard@gmail.com>
2024-04-13 18:08:32 -0700

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -16,7 +16,7 @@ import SampleList from "@/components/SampleList";
 
 const sampleEventId =
   process.env.NODE_ENV === "production"
-    ? "z45bhxefzma9"
+    ? "nedqaur6b3fg"
     : "clpiq9ohe0005vt8uksvndxq3";
 const sampleEventListId = "cloetmfc60001jr0ays7hsxum";
 
