340bd8216f8eca15a718e50ce6f48d2dd53e6095
Jaron Heard <jaronheard@gmail.com>
2024-04-14 17:23:08 -0700

    upgrade next and remove vercel toolbar

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const dotenvExpand = require("dotenv-expand");
 
 dotenvExpand.expand({ parsed: { ...process.env } });
@@ -30,24 +31,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { webpack }) => {
-    // copied directly from the sentry treeshaking guide
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    config.plugins.push(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-      new webpack.DefinePlugin({
-        // __SENTRY_DEBUG__: false,
-        // __SENTRY_TRACING__: false,
-        // __RRWEB_EXCLUDE_IFRAME__: true,
-        // __RRWEB_EXCLUDE_SHADOW_DOM__: true,
-        // __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
-      })
-    );
-
-    // return the modified config
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return config;
-  },
   async rewrites() {
     return process.env.NODE_ENV === "production"
       ? {
@@ -77,44 +60,40 @@ const nextConfig = {
   },
 };
 
-const withVercelToolbar = require("@vercel/toolbar/plugins/next")();
 // Injected content via Sentry wizard below
-
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withVercelToolbar(
-  withSentryConfig(
-    nextConfig,
-    {
-      // For all available options, see:
-      // https://github.com/getsentry/sentry-webpack-plugin#options
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-      // Suppresses source map uploading logs during build
-      silent: true,
-      org: "soonlist",
-      project: "soonlist",
-    },
-    {
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "soonlist",
+    project: "soonlist",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-      // Transpiles SDK to be compatible with IE11 (increases bundle size)
-      transpileClientSDK: false,
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
 
-      // Hides source maps from generated client bundles
-      hideSourceMaps: true,
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
 
-      // Enables automatic instrumentation of Vercel Cron Monitors.
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    }
-  )
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  }
 );
